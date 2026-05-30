3b979036bfa47754cdb999f840a0e43187d769fe
Keyner-H <keynerjo.hernandezla@ecci.edu.co>
2026-05-29 23:28:28 -0500

    Horca

--- Codigo ahorcado.py
+++ Codigo ahorcado.py
@@ -1,15 +1,31 @@
 import turtle
 import random
 
-# 1. Configuración inicial de la pantalla
+#Configuración inicial de la pantalla
 pantalla = turtle.Screen()
 pantalla.title("Juego del Ahorcado")
 pantalla.bgcolor("white")
 pantalla.setup(width=600, height=600)
 
 t = turtle.Turtle()
-t.hideturtle() # Ocultamos la flechita
-t.speed(3)     # Velocidad del dibujo
-t.pensize(3)   # Grosor de la línea
+t.hideturtle()
+t.speed(3)
+t.pensize(3)
 
-turtle.bye() # Cierra la ventana (temporalmente para probar)
+# Dibujar la base
+def dibujar_horca():
+    t.penup()
+    t.goto(-150, -150)
+    t.pendown()
+    t.forward(300) # Base
+    t.backward(150)
+    t.left(90)
+    t.forward(300) # Poste vertical
+    t.right(90)
+    t.forward(150) # Techo
+    t.right(90)
+    t.forward(40)  # Cuerda
+
+# Probando la función
+dibujar_horca()
+turtle.bye()
